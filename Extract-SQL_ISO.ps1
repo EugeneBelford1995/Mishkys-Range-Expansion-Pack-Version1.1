19c25d73e146fff4ebc9f6964306d2a7ed085d1f
Rich <114684848+EugeneBelford1995@users.noreply.github.com>
2025-05-06 20:35:54 -0400

    Update Extract-SQL_ISO.ps1
    Found a good link to pull the ISO from, commented out the previous link.

--- Extract-SQL_ISO.ps1
+++ Extract-SQL_ISO.ps1
@@ -2,6 +2,8 @@
 #Save it as C:\VM_Stuff_Share\ISOs\SQLServer2022-x64-ENU.iso
 #Run this part on the hypervisor to extract SQL from the ISO and then create the Zip
 #Mishky's AD Range uploads the ZIP to a VM, intalls, & configures MSSQL automatically
+#Invoke-WebRequest "https://go.microsoft.com/fwlink/?linkid=2215202&clcid=0x409&culture=en-us&country=us" -OutFile "C:\VM_Stuff_Share\ISOs\SQLServer2022-x64-ENU.iso"
+Invoke-WebRequest -Uri "https://archive.org/download/enu_sql_server_2022_standard_edition_x64_dvd_43079f69/enu_sql_server_2022_standard_edition_x64_dvd_43079f69.iso" -OutFile "C:\VM_Stuff_Share\ISOs\SQLServer2022-x64-ENU.iso"
 New-Item -Path "C:\VM_Stuff_Share\SQL2022" -ItemType Directory
 $mountResult = Mount-DiskImage -ImagePath 'C:\VM_Stuff_Share\ISOs\SQLServer2022-x64-ENU.iso' -PassThru
 $volumeInfo = $mountResult | Get-Volume
